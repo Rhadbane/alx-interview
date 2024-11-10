72a9b8054a9168af338d77035173bfb2eeaf813a
Rhadbane <rhadbane.idriss@gmail.com>
2024-11-10 12:19:20 +0100

    new

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,74 +1,48 @@
 #!/usr/bin/python3
-"""
-N queens solution - place N non-attacking queens on an NxN chessboard
-"""
+""" Script for the N-Queens Challenge """
+
 import sys
 
 
-def is_safe(board, row, col):
-    """Check if a queen can be placed on board[row][col]"""
-    # Check row on left side
-    for x in range(col):
-        if board[row][x] == 1:
-            return False
+def queen_down(N, i, queens, final_combo, col, pos, neg):
+    """recursive function to put down non attacking queens"""
 
-    # Check upper diagonal on left side
-    for x, y in zip(range(row, -1, -1),
-                   range(col, -1, -1)):
-        if board[x][y] == 1:
-            return False
+    if len(queens) == N:
+        final_combo.append(queens)
+        return final_combo
 
-    # Check lower diagonal on left side
-    for x, y in zip(range(row, N, 1),
-                   range(col, -1, -1)):
-        if board[x][y] == 1:
-            return False
-
-    return True
-
-
-def solve_nqueens(board, col):
-    """Solve N queens problem using backtracking"""
-    # Base case: If all queens are placed, return True
-    if col >= N:
-        solution = []
-        for i in range(N):
-            for j in range(N):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        print(solution)
-        return
-
-    # Consider this column and try placing this queen in all rows one by one
-    for i in range(N):
-        if is_safe(board, i, col):
-            # Place this queen in board[i][col]
-            board[i][col] = 1
-            
-            # Make recursive call to place rest of the queens
-            solve_nqueens(board, col + 1)
-            
-            # If placing queen in board[i][col] doesn't lead to a solution,
-            # then remove queen from board[i][col]
-            board[i][col] = 0
+    for j in range(N):
+        if not (j in col or i + j in pos or i - j in neg):
+            queen_down(
+                N,
+                i + 1,
+                queens + [[i, j]],
+                final_combo,
+                col + [j],
+                pos + [i + j],
+                neg + [i - j],
+            )
+    return final_combo
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    # Initialize empty NxN chessboard
-    board = [[0 for x in range(N)] for y in range(N)]
-    
-    solve_nqueens(board, 0)
+queens = []
+col = pos = neg = []
+
+queen_down(N, 0, [], queens, col, pos, neg)
+
+for queen in queens:
+    print(queen)
